9c341adcd2716e3e7f20eee0bb479d83ebf7de9a
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-21 14:56:24 +0200

    added series section and some extra case in the function to get the flag

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -17,6 +17,12 @@ import { store } from '../store.js';
                     countryCode = 'cn'
                 }else if(countryCode === 'ko'){
                     countryCode = 'kr'
+                }else if(countryCode === 'hi'){
+                    countryCode = 'in'
+                }else if(countryCode === 'cs'){
+                    countryCode = 'cz'
+                }else if(countryCode === undefined){
+                    countryCode = ''
                 }
                 const codePoints = countryCode.toUpperCase().split('').map(char =>  127397 + char.charCodeAt());
                 return String.fromCodePoint(...codePoints);
@@ -28,11 +34,19 @@ import { store } from '../store.js';
 
 <template>
     <main>
+        <h1 v-if="store.movieRes.length > 0">Film trovati</h1>
         <div class="movieInfo" v-for="movie in store.movieRes">
             <p>{{movie.original_title}}</p>
             <p>{{movie.title}}</p>
             <p>{{getFlagEmoji(movie.original_language)}}</p>
             <p>{{movie.vote_average}}</p>
+        </div>
+        <h1 v-if="store.seriesRes.length > 0">Serie trovate</h1>
+        <div class="seriesInfo" v-for="series in store.seriesRes">
+            <p>{{series.original_name}}</p>
+            <p>{{series.name}}</p>
+            <p>{{getFlagEmoji(series.original_language)}}</p>
+            <p>{{series.vote_average}}</p>
         </div>
     </main>
 </template>
