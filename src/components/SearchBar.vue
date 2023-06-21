8c4c500542bd0fcf19c845e2efc1de1f63015038
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-21 13:02:53 +0200

    added searchbar that activate api request for the movie with same name as search

---
+++ src/components/SearchBar.vue
@@ -0,0 +1,22 @@
+<script>
+    export default {
+        data() {
+            return {
+                searchText: '',
+            }
+        },
+    }
+</script>
+
+<template>
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+                <input class="form-control me-2" type="search" placeholder="Titolo del film" aria-label="Search" v-model="searchText" @keyup.enter="$emit('seachMovie', searchText)">
+                <button class="btn btn-outline-success" type="send" @click="$emit('seachMovie', searchText), searchText=''" >Cerca</button>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
