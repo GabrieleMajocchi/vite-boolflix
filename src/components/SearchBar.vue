fd0d8bff883d8938f99cabc7249ec472f8dd1d9b
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-26 03:15:11 +0200

    added function to get genres and some events on use of searchbar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -5,7 +5,7 @@ import { store } from '../store.js';
         data() {
             return {
                 searchText: '',
-                apiUrl: 'https://api.themoviedb.org/3/search/',
+                apiUrl: 'https://api.themoviedb.org/3/',
                 apiKey: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMWVmNTA0ZDMyZjUxMDBhMzM2YTQwOGNlNDNhZTY1YiIsInN1YiI6IjY0OTJjODU5NzA2ZTU2MDBhZDFhMmVmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.5O5CHPkuc11G8l5WEOUiRp32ubkmdocptlVSPLLbliU',
                 accessToken: 'api_key=01ef504d32f5100a336a408ce43ae65b',
                 store,
@@ -13,25 +13,40 @@ import { store } from '../store.js';
         },
         methods: {
             getMovies(search){
-                axios.get(`${this.apiUrl}movie?${this.accessToken}&query=${search}`)
+                axios.get(`${this.apiUrl}search/movie?${this.accessToken}&query=${search}`)
                 .then((response)=>{
                     // console.log(response.data.results);
                     this.store.movieRes = response.data.results;
                 })
-                axios.get(`${this.apiUrl}tv?${this.accessToken}&query=${search}`)
+                axios.get(`${this.apiUrl}search/tv?${this.accessToken}&query=${search}`)
                 .then((response)=>{
                     // console.log(response.data.results);
                     this.store.seriesRes = response.data.results;
                 })
             },
+            getGenres(){
+                axios.get(`${this.apiUrl}genre/movie/list?language=en%27&${this.accessToken}`)
+                .then((response)=>{
+                    // console.log(response.data.genres);
+                    this.store.movieGen = response.data.genres;
+                })
+                axios.get(`${this.apiUrl}genre/tv/list?language=en%27&${this.accessToken}`)
+                .then((response)=>{
+                    // console.log(response.data.genres);
+                    this.store.seriesTv = response.data.genres;
+                })
+            }
         },
+        created(){
+            this.getGenres()
+        }
     }
 </script>
 
 <template>
     <div class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Titolo del film" aria-label="Search" v-model="searchText" @keyup.enter="getMovies(searchText)">
-        <button class="btn btn-outline-danger" type="button" @click="getMovies(searchText), searchText=''" >Cerca</button>
+        <input class="form-control me-2" type="search" placeholder="Movie or Series Title" aria-label="Search" v-model="searchText" @keyup.enter="getMovies(searchText), store.userSelection = 'search',  this.store.trailerUrl=''">
+        <button class="btn btn-outline-danger" type="button" @click="getMovies(searchText), searchText='', store.userSelection = 'search',  this.store.trailerUrl=''" >Search</button>
     </div>
 </template>
 
