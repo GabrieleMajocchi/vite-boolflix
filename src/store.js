2cfa936fd17548add62f9b55831df5fc11d3e2ee
GabrieleMajocchi <gabriele.majocchi@gmail.com>
2023-06-26 03:12:04 +0200

    added some global variables

--- src/store.js
+++ src/store.js
@@ -3,4 +3,12 @@ import { reactive } from 'vue';
 export const store = reactive({
     movieRes: [],
     seriesRes: [],
+    movieGen: [],
+    seriesTv: [],
+    userSelection: '',
+    popularMovies: [],
+    popularSeries: [],
+    trandingMovies: [],
+    trandingSeries: [],
+    trailerUrl: '',
 });
